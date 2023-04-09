53125a4f1613e1bcd659045c19667102a6daf2d7
viktorijagrietniece <123946342+viktorijagrietniece@users.noreply.github.com>
2023-04-09 22:50:26 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,45 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+
+    input_type = input().rstrip()
+    if input_type == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == 'F':
+        with open("./tests/" + filename, mode "r") as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+ 
+    return (pattern, text)
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
-
-    # and return an iterable variable
-    return [0]
-
+    occurrences = []
+    p = len(pattern)
+    t = len(text)
+    prime = 101
+    base = 256
+    mod = 10**9 + 7
+    hash_pattern = 0
+    hash_text = 0
+    power = 1
+    
+    for i in range(p):
+       hash_pattern = (hash_pattern * base + ord(pattern[i])) % mod
+       hash_text = (hash_text * base + ord(text[i])) % mod
+       power = (power * base) % mod
+    for i in range(t - p + 1):
+         if hash_pattern == hash_text and text[i:i + p] == pattern:
+             occurrences.append(i)
+         if i < t - p:
+             hash_text = (base * (hash_text - ord(text[i]) * power) + ord(text[i + p])) % mod
+             hash_text = (hash_text + mod) % mod
+    return occurrences
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
